a9e9e71248fb936804834b7a5c01a039540fbf88
Edracoon <edgar.pfennig@gmail.com>
2023-10-29 11:45:33 +0100

    init and .env int gitignore

---
+++ swifty-companion/swifty-companion/swifty_companionApp.swift
@@ -0,0 +1,17 @@
+//
+//  swifty_companionApp.swift
+//  swifty-companion
+//
+//  Created by Edgar Pfennig on 29/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct swifty_companionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
